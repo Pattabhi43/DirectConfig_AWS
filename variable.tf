9ed939bad1b397986f9b8fad6d8adf43adccb5f5
Pattabhi43 <kpramaiah46@gmail.com>
2023-09-06 18:49:07 +0530

    Hardcoded variables into variables.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,13 @@
 variable "AWS_access_key" {
+  default = "AKIAXUXSKSCYUFS24TMU"
 }
 
 variable "SECRET_key" {
+  default = "HaZYSOINnIY4gXgMIoXOuehTm7TlIGCIt9cROq75"
 }
 
 variable "redhat_ami" {
+  default = "ami-026ebd4cfe2c043b2"
 }
 
 variable "instance_type" {
